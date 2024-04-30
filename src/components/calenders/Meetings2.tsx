254e22326f038ac511a26918ded295c1c69d5021
Dharamveer singh <dharamveersingh.em@gmail.com>
2024-04-30 21:12:16 +0530

    Add @radix-ui/react-popover package and implement Popover component

--- src/components/calenders/Meetings2.tsx
+++ src/components/calenders/Meetings2.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import {
   QueryClient,
   QueryClientProvider,
@@ -5,9 +6,7 @@ import {
 } from "@tanstack/react-query";
 import type { CollectionEntry } from "astro:content";
 import { ArrowUpRight } from "lucide-react";
-import React, { useEffect } from "react";
-import { Calendar } from "../ui/calendar";
-import { Switch } from "../ui/switch";
+import { DatePickerWithRange } from "./DateRangePicker";
 
 interface Meeting {
   summary: string;
@@ -39,9 +38,6 @@ const Meetings = ({
 
 export default Meetings;
 
-// merge:{
-//   "17/11/2023" :[]
-// }
 const mergeSameDates = (arr: any) => {
   const merged: any = {};
   arr?.forEach((event: any) => {
@@ -69,147 +65,85 @@ const CustomCalendar = ({
   type?: string;
   calendarId?: string;
 }) => {
-  const [date, setDate] = React.useState<Date | null>(null);
-  const [month, setMonth] = React.useState<Date | null>(
+  const [date, setDate] = useState<Date | null>(null);
+
+  const [month, setMonth] = useState<Date | null>(
     new Date(new Date().getFullYear(), new Date().getMonth(), 1),
   );
-  const [events, setEvents] = React.useState<Record<
+
+  const [events, setEvents] = useState<Record<string, Array<Meeting>> | null>(
+    null,
+  );
+
+  const [eventsByMonth, setEventsByMonth] = useState<Record<
     string,
     Array<Meeting>
   > | null>(null);
-  const [eventsByMonth, setEventsByMonth] = React.useState<Record<
-    string,
-    Array<Meeting>
-  > | null>(null);
-  const [booked, setBooked] = React.useState<Date[]>([]);
 
-  const [recourses, setRecourses] = React.useState<boolean>(false);
-  const { isLoading, data, isFetching } = useQuery({
-    queryKey: ["events", type],
-    queryFn: async () => {
-      const response = await fetch(
-        `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?key=AIzaSyAxCoI_edYVZ1xA9qCclqHv-Ji19J08c5Q`,
-      );
-      const data = await response.json();
+  const [booked, setBooked] = useState<Date[]>([]);
 
-      // if (type) {
-      //   const items = data?.items?.filter((event: any) => {
-      //     return event.summary?.toLowerCase().includes(type.toLowerCase());
-      //   });
-      //   return {
-      //     ...data,
-      //     items,
-      //   };
-      // }
+  const [recourses, setRecourses] = useState<boolean>(true);
 
-      return data;
-    },
-  });
   // console.log(res);
-
-  React.useEffect(() => {
-    if (data?.items) {
-      setBooked(
-        data?.items?.map((date: any) => {
-          return new Date(date.start.dateTime);
-        }),
-      );
-    }
-  }, [data]);
 
   useEffect(() => {
     if (date) {
-      if (recourses) {
-        setEvents(
-          mergeSameDates(
-            meetings
-              ?.filter((event) => {
-                const eventDate = event?.data?.date;
-                return (
-                  eventDate?.getFullYear() === date.getFullYear() &&
-                  eventDate?.getMonth() === date.getMonth() &&
-                  eventDate?.getDate() === date.getDate()
-                );
-              })
-              .map((event) => {
-                return {
-                  summary: event.data.title,
-                  start: {
-                    dateTime: event?.data.date.toISOString(),
-                  },
-                  slug: event.slug,
-                };
-              }),
-          ),
-        );
-      } else {
-        setEvents(
-          mergeSameDates(
-            data?.items?.filter((event: any) => {
-              const eventDate = new Date(event.start.dateTime);
+      setEvents(
+        mergeSameDates(
+          meetings
+            ?.filter((event) => {
+              const eventDate = event?.data?.date;
               return (
-                eventDate.getFullYear() === date.getFullYear() &&
-                eventDate.getMonth() === date.getMonth() &&
-                eventDate.getDate() === date.getDate()
+                eventDate?.getFullYear() === date.getFullYear() &&
+                eventDate?.getMonth() === date.getMonth() &&
+                eventDate?.getDate() === date.getDate()
               );
-            }),
-          ),
-        );
-      }
-    }
-  }, [date, data, recourses]);
-
-  useEffect(() => {
-    if (month) {
-      if (recourses) {
-        const e = meetings?.filter((event) => {
-          const eventDate = event?.data?.date;
-          return (
-            eventDate?.getFullYear() === month.getFullYear() &&
-            eventDate?.getMonth() === month.getMonth()
-          );
-        });
-
-        const sorted = e?.sort((a, b) => {
-          return (
-            new Date(a?.data?.date).getTime() -
-            new Date(b?.data?.date).getTime()
-          );
-        });
-
-        setEventsByMonth(
-          mergeSameDates(
-            sorted?.map((event) => {
+            })
+            .map((event) => {
               return {
-                summary: event?.data?.title,
+                summary: event.data.title,
                 start: {
-                  dateTime: event?.data?.date.toISOString(),
+                  dateTime: event?.data.date.toISOString(),
                 },
                 slug: event.slug,
               };
             }),
-          ),
-        );
-      } else {
-        const e = data?.items?.filter((event: any) => {
-          const eventDate = new Date(event.start.dateTime);
-          return (
-            eventDate.getFullYear() === month.getFullYear() &&
-            eventDate.getMonth() === month.getMonth()
-          );
-        });
-
-        const sorted = e?.sort((a: any, b: any) => {
-          return (
-            new Date(a.start.dateTime).getTime() -
-            new Date(b.start.dateTime).getTime()
-          );
-        });
-
-        setEventsByMonth(mergeSameDates(sorted));
-      }
+        ),
+      );
     }
-  }, [month, data, recourses]);
+  }, [date]);
+
+  useEffect(() => {
+    if (month) {
+      const e = meetings?.filter((event) => {
+        const eventDate = event?.data?.date;
+        return (
+          eventDate?.getFullYear() === month.getFullYear() &&
+          eventDate?.getMonth() === month.getMonth()
+        );
+      });
+
+      const sorted = e?.sort((a, b) => {
+        return (
+          new Date(a?.data?.date).getTime() - new Date(b?.data?.date).getTime()
+        );
+      });
+
+      setEventsByMonth(
+        mergeSameDates(
+          sorted?.map((event) => {
+            return {
+              summary: event?.data?.title,
+              start: {
+                dateTime: event?.data?.date.toISOString(),
+              },
+              slug: event.slug,
+            };
+          }),
+        ),
+      );
+    }
+  }, [month]);
 
   console.log(recourses);
 
@@ -231,26 +165,37 @@ const CustomCalendar = ({
                   })
                 : "Select a date"}
           </h1>
+          <DatePickerWithRange />
         </div>
         <div className="custom-scrollbar flex flex-col gap-4 overflow-y-auto pb-5 pt-4 md:max-h-[20.5rem] md:pb-0">
-          {isLoading || isFetching ? (
-            <Loading />
-          ) : (
-            <>
-              {eventsByMonth &&
-                month &&
-                Object?.keys(eventsByMonth).length === 0 && (
-                  <p className=" pb-5 text-center text-sm font-medium">
-                    No events found for this month
-                  </p>
-                )}
-
-              {date && events && Object?.keys(events).length === 0 && (
-                <p className=" text-center text-sm font-medium">
-                  No events found for this day
+          <>
+            {eventsByMonth &&
+              month &&
+              Object?.keys(eventsByMonth).length === 0 && (
+                <p className=" pb-5 text-center text-sm font-medium">
+                  No events found for this month
                 </p>
               )}
-              {Object?.keys(eventsByMonth)?.map((date, i) => (
+
+            {date && events && Object?.keys(events).length === 0 && (
+              <p className=" text-center text-sm font-medium">
+                No events found for this day
+              </p>
+            )}
+            <div className="grid grid-cols-5 gap-2 border-b px-5 pb-4">
+              {["Meeting", "Time", "Notes", "Transcript", "Recording"]?.map(
+                (item, index) => (
+                  <h2
+                    key={index}
+                    className="text-xs font-medium text-zinc-500 md:text-sm"
+                  >
+                    {item}
+                  </h2>
+                ),
+              )}
+            </div>
+            {eventsByMonth &&
+              Object?.keys(eventsByMonth)?.map((date, i) => (
                 <div key={i} className="flex flex-col gap-4 border-b px-5 pb-5">
                   <h1 className="text-sm font-medium">{date}</h1>
                   {eventsByMonth[date]?.map((event, index) => (
@@ -263,8 +208,7 @@ const CustomCalendar = ({
                   ))}
                 </div>
               ))}
-            </>
-          )}
+          </>
         </div>
       </div>
     </div>
@@ -281,66 +225,49 @@ const Event = ({
   type?: string;
 }) => {
   return (
-    <div key={event.id} className="flex items-center justify-between px-2 ">
-      <div className="flex  gap-8">
-        <span className=" w-20 text-xs font-medium text-zinc-500 md:text-sm">
-          {new Date(event.start.dateTime).toLocaleTimeString("en-US", {
-            hour: "numeric",
-            minute: "2-digit",
-            hour12: false,
-          })}{" "}
-          PST
-        </span>
-        <a
-          href={
-            recourses
-              ? type
-                ? `/working-groups/${event?.slug}/#sub`
-                : `/community-hub/initiatives/meetings/${event?.slug}/#sub`
-              : event?.htmlLink
-          }
-        >
-          <h2 className="text-xs font-medium text-primary hover:underline md:text-sm">
-            {event.summary}
-          </h2>
-        </a>
-      </div>
-      {recourses && (
-        <a
-          href={
-            type
-              ? `/working-groups/${event?.slug}/#sub`
-              : `/community-hub/initiatives/meetings/${event?.slug}/#sub`
-          }
-          className="flex items-center gap-1 font-os text-xs  text-zinc-500"
-        >
-          Resources
-          <ArrowUpRight size={16} />
-        </a>
-      )}
-    </div>
-  );
-};
-
-const Loading = () => {
-  return (
-    <div role="status" className="w-full animate-pulse px-5 ">
-      <div className="mb-2 h-3 w-10 rounded bg-zinc-200 dark:bg-zinc-700"></div>
-      <div className="mb-4 h-5 w-1/3 rounded bg-zinc-200 dark:bg-zinc-700"></div>
-      <div className="my-4 h-px w-full rounded bg-zinc-200 dark:bg-zinc-700"></div>
-
-      <div className="mb-2 h-3 w-10 rounded bg-zinc-200 dark:bg-zinc-700"></div>
-      <div className="mb-4 h-5 w-1/3 rounded bg-zinc-200 dark:bg-zinc-700"></div>
-      <div className="my-4 h-px w-full rounded bg-zinc-200 dark:bg-zinc-700"></div>
-
-      <div className="mb-2 h-3 w-10 rounded bg-zinc-200 dark:bg-zinc-700"></div>
-      <div className="mb-4 h-5 w-1/3 rounded bg-zinc-200 dark:bg-zinc-700"></div>
-      <div className="my-4 h-px w-full rounded bg-zinc-200 dark:bg-zinc-700"></div>
-      <div className="mb-2 h-3 w-10 rounded bg-zinc-200 dark:bg-zinc-700"></div>
-      <div className="mb-4 h-5 w-1/3 rounded bg-zinc-200 dark:bg-zinc-700"></div>
-      <div className="my-4 h-px w-full rounded bg-zinc-200 dark:bg-zinc-700"></div>
-
-      <span className="sr-only">Loading...</span>
+    <div key={event.id} className="grid grid-cols-5 gap-2">
+      <h1 className="font-medium">{event.summary}</h1>
+      <p className="font-os text-xs text-zinc-400">
+        {new Date(event.start.dateTime).toLocaleTimeString("en-US", {
+          hour: "numeric",
+          minute: "2-digit",
+          hour12: false,
+        })}{" "}
+        PST
+      </p>
+      <a
+        href={
+          type
+            ? `/working-groups/${event?.slug}/#sub`
+            : `/community-hub/initiatives/meetings/${event?.slug}/#sub`
+        }
+        className="flex gap-1 font-os text-xs  "
+      >
+        Notes
+        <ArrowUpRight size={16} />
+      </a>
+      <a
+        href={
+          type
+            ? `/working-groups/${event?.slug}/#sub`
+            : `/community-hub/initiatives/meetings/${event?.slug}/#sub`
+        }
+        className="flex gap-1 font-os text-xs  "
+      >
+        Transcript
+        <ArrowUpRight size={16} />
+      </a>
+      <a
+        href={
+          type
+            ? `/working-groups/${event?.slug}/#sub`
+            : `/community-hub/initiatives/meetings/${event?.slug}/#sub`
+        }
+        className="flex gap-1 font-os text-xs  "
+      >
+        View Recording
+        <ArrowUpRight size={16} />
+      </a>
     </div>
   );
 };
