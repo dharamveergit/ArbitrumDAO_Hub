be19cf678925be40e57c4b9c1ee3a8540cd8cc56
Dharamveer singh <dharamveersingh.em@gmail.com>
2024-05-04 00:17:19 +0530

    Refactor info-cards-wrapper and meetings/index components

---
+++ src/data/homepage/getting-started.ts
@@ -0,0 +1,134 @@
+import { GITHUB_URL } from "@/consts";
+
+export const gettingStarted = {
+  title: "Get Started with Arbitrum Hub",
+  description:
+    "Welcome to the Arbitrum Hub - your ultimate gateway to navigating the Arbitrum DAO Ecosystem! Here, you'll find everything you need to dive into the world of Arbitrum DAO, from understanding its decentralized governance to exploring its vibrant community and innovative projects.",
+  buttons: [
+    {
+      text: "Add to Github",
+      link: GITHUB_URL,
+      type: "default",
+    },
+    {
+      text: "How to contribute",
+      link: "#how-to-contribute",
+      type: "secondary",
+    },
+  ],
+  cards: [
+    {
+      title: "Comprehensive Information",
+      description:
+        "Dive deep into the Arbitrum ecosystem with curated resources, guides, and tutorials to help you understand the ins and outs of decentralized finance (DeFi) on Arbitrum.",
+      image: "/images/incentive-hub/getting-started/eye.svg",
+    },
+    {
+      title: "Engage with the Community",
+      description:
+        "Connect with fellow enthusiasts, developers, and stakeholders in the Arbitrum community hub. Join discussions, share ideas, and collaborate on projects shaping the future of DeFi.",
+      image: "/images/incentive-hub/getting-started/person.svg",
+    },
+    {
+      title: "Developer Resources",
+      description:
+        "Explore a treasure trove of developer tools, frameworks, and documentation to kickstart your journey in building decentralized applications (DApps) on Arbitrum.",
+      image: "/images/incentive-hub/getting-started/person.svg",
+    },
+    {
+      title: "Grants and Incentives",
+      description:
+        "Discover opportunities to fund your projects and initiatives through Arbitrum's grants programs and incentive initiatives. Learn how to apply, participate, and contribute to the growth of the ecosystem.",
+      image: "/images/incentive-hub/getting-started/card.svg",
+    },
+  ],
+  programs: [
+    {
+      title: "Community Hub",
+      subtitle: "Connect, Collaborate, Innovate",
+      description:
+        "The Community Hub is your central spot for all things Arbitrum. Explore resources, connect with others, and stay updated on events and initiatives. Dive into collaboration and innovation here!",
+      link: "/community-hub/the-hub/",
+    },
+    {
+      title: "Developer Hub",
+      subtitle: "Build, Innovate, Create",
+      description:
+        "The Developer Hub is your gateway to the full potential of Arbitrum. Access resources, tools, and tutorials to streamline development. Collaborate with developers, stay updated and build the future of decentralized applications.",
+      link: "/developer-hub",
+    },
+    {
+      title: "Grant Hub",
+      subtitle: "Empowering Innovation, Fueling Growth",
+      description:
+        "The Arbitrum Grant Hub empowers innovation and fuels growth in the ecosystem. Explore grant programs, objectives, and application processes. Dive into insights on funded projects, accelerating development and adoption.",
+      link: "/grant-hub",
+    },
+    {
+      title: "Incentive Hub",
+      subtitle: "Fostering Innovation, Driving Adoption",
+      description:
+        "Discover a curated collection of incentive programs designed to spur growth and reward contributions. Learn about the objectives, benefits, and application processes of each program, and find out how you can get involved.",
+      link: "/incentive-programs/",
+    },
+    {
+      title: "Proposal Hub",
+      subtitle: "Empowering Community Decision-Making",
+      description:
+        "Discover proposals to enhance functionality, security, and growth. Engage, vote, and track history transparently for inclusive governance.",
+      link: "/proposal-hub",
+    },
+    {
+      title: "Working Groups",
+      subtitle: "Driving Progress, Together",
+      description:
+        "Engage with specialized teams, schedules, and resources. Shape Arbitrum's future through active participation in the Working Groups Hub.",
+      link: "/working-groups",
+    },
+    {
+      title: "Ecosystem",
+      subtitle: "Explore Innovation, Fuel Growth",
+      description:
+        "The vibrant Arbitrum Ecosystem Hub. Discover a diverse range of decentralized applications (DApps) and grant projects fostering innovation and growth. Dive into brief descriptions and relevant links.",
+      link: "/ecosystem-hub",
+    },
+    {
+      title: "Arbitrum DAO’s Multisig",
+      subtitle: "Empowering Decision-Making",
+      description:
+        "Gain insight into financial and performance metrics on the Arbitrum DAO Multisig Hub. Access treasury holdings, grant allocations, cash flow, and transaction history for strategic decision-making and transparent governance.",
+      link: "/multi-sigs",
+    },
+  ],
+  questions: [
+    {
+      question:
+        "How can I contribute to the development of the Arbitrum Hub platform?",
+      answer: `<p>You can contribute to the Arbitrum Hub platform by actively participating in discussions, providing feedback on existing features, suggesting improvements, and even contributing code if you're a developer. Your contributions help enhance the platform and make it more valuable for the community.</p>`,
+    },
+    {
+      question: "Is the Arbitrum Hub platform open-source?",
+      answer: `<p>Yes, the Arbitrum Hub platform is open-source. You can access the codebase on platforms like GitHub, where you can review the code, suggest changes, and even fork the repository to contribute to its development. Transparency and collaboration are key principles guiding the platform's evolution.</p>`,
+    },
+    {
+      question:
+        "Where can I report inaccuracies or issues I encounter on the Arbitrum Hub platform?",
+      answer: `<p>If you come across any inaccuracies or issues while using the Arbitrum Hub platform, you can report them through the designated channels provided on the platform. This could include submitting a bug report, reaching out to the support team, or engaging with the community to discuss and address the issue collaboratively.</p>`,
+    },
+    {
+      question:
+        "Who is responsible for ensuring the accuracy of data on the Arbitrum Hub platform?",
+      answer: `<p>Responsibility for data accuracy on the Arbitrum Hub platform lies with the community. We facilitate contributions but do not take sides. Maintenance primarily relies on community input, with our role limited to implementing suggested changes.</p>`,
+    },
+    {
+      question:
+        "How can I stay informed about the latest updates and developments in the Arbitrum ecosystem?",
+      answer: `<p>Stay up-to-date with the latest news, announcements, and insights from the Arbitrum team, developers, and industry experts by regularly visiting the Blog Hub section of the Arbitrum Hub website. Subscribe to newsletters, follow social media channels, and join community forums to receive real-time updates and engage in discussions about the ecosystem.</p>`,
+    },
+    {
+      question:
+        "How can I access financial and performance data of the Arbitrum network to inform my strategic decision-making?",
+      answer: `<p>Utilize the Arbitrum DAO Multisig Hub to access comprehensive financial and performance insights of the network, including treasury holdings, grant allocations, cash flow, and transaction history. Analyze this data to gain valuable insights, assess network performance, and make informed decisions about your participation in the ecosystem.</p>`,
+    },
+  ],
+};
