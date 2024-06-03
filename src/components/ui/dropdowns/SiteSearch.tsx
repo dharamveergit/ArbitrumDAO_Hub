3ee41470a45520df65e0bf05a7665a249153bf2d
Dharamveer singh <dharamveersingh.em@gmail.com>
2024-06-04 01:15:08 +0530

    chore: Update Keystatic config to include new EventsA collection

--- src/components/ui/dropdowns/SiteSearch.tsx
+++ src/components/ui/dropdowns/SiteSearch.tsx
@@ -1,33 +1,34 @@
-import React from "react";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { Search } from "lucide-react";
-import S from "astro-pagefind/components/Search";
+import clsx from "clsx";
+import { useEffect, useState } from "react";
+
 const SiteSearch = () => {
+  const [isInputHasValue, setIsInputHasValue] = useState(false);
+  const [isThereResults, setIsThereResults] = useState(false);
+  // find value of this class pagefind-ui__search-input
+  //  in pagefind-ui__results has children when search change
+
+  useEffect(() => {
+    const searchInput = document.querySelector(".pagefind-ui__search-input");
+    if (searchInput) {
+      searchInput.addEventListener("input", (e: any) => {
+        setIsInputHasValue(e.target.value.length > 0);
+      });
+    }
+  }, []);
+
   return (
-    <Dialog>
-      <DialogTrigger>
-        {" "}
-        <Search height={16} width={16} />
-      </DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Search</DialogTitle>
-          <DialogDescription>Search for anything on the site</DialogDescription>
-          <S
-            id="search"
-            className="pagefind-ui"
-            uiOptions={{ showImages: false }}
-          />
-        </DialogHeader>
-      </DialogContent>
-    </Dialog>
+    <div
+      id="search-no-results"
+      className={clsx(
+        "flex flex-col items-center justify-center gap-8 px-3 pb-4",
+        isInputHasValue ? "hidden" : "flex",
+      )}
+    >
+      <p className="text-center text-sm text-gray-500">
+        {isInputHasValue ? "No results found" : "Start typing to search"}
+      </p>
+      <img src="/images/empty3.svg" alt="Arbitrum Logo" className="w-1/2" />
+    </div>
   );
 };
 
