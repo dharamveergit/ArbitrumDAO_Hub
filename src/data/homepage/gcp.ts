9489dc53ab51346b834813989892b857d9c16270
dharamveergit <dharamveersingh.em@gmail.com>
2024-10-12 15:06:37 +0530

    update gcp.ts and gcp.astro

--- src/data/homepage/gcp.ts
+++ src/data/homepage/gcp.ts
@@ -68,4 +68,60 @@ export const GCP_Homepage = {
         "<p >The Arbitrum developer docs offer troubleshooting guides and community forums provide support. Consider using tools like Etherscan and Hardhat debugging functionalities.</p>",
     },
   ],
+  gameFi: [
+    {
+      title: "Army of Tactics",
+
+      description:
+        "Embark on an exhilarating journey through the world of Army of Tactics, where strategy and fortune blend into the best mobile game experience. Free to download and thrilling to play, your saga begins on mystical islands, with every battle a step towards glory!",
+      link: "https://portal.arbitrum.io/projects/gaming?project=army-of-tactics",
+      image:
+        "https://portal.arbitrum.io/_next/image?url=%2Fimages%2Fprojects%2Farmy-of-tactics-logo.webp&w=256&q=75",
+    },
+
+    {
+      commingsoon: true,
+      title: "Army of Tactics",
+      description:
+        "Embark on an exhilarating journey through the world of Army of Tactics, where strategy and fortune blend into the best mobile game experience. Free to download and thrilling to play, your saga begins on mystical islands, with every battle a step towards glory!",
+      link: "https://portal.arbitrum.io/projects/gaming?project=avarik-saga",
+      image:
+        "https://portal.arbitrum.io/_next/image?url=%2Fimages%2Fprojects%2Favarik-saga-logo.webp&w=256&q=75",
+    },
+    {
+      commingsoon: true,
+      title: "Captain Laserhawk The GAME",
+      description:
+        "Captain Laserhawk: the G.A.M.E. expands the universe of Captain Laserhawk: A Blood Dragon Remix series - produced by Ubisoft for Netflix and featuring iconic characters from Ubisoft IPs. The G.A.M.E. is a transmedia gaming adventure where the community drives the narrative through innovative governance. Players step into the role of a citizen in the dystopian world of Eden - the totalitarian successor to the US. By overcoming challenges across various channels inside and outside the game, players can raise their citizen score and increase their power to shape the narrative. As the story unfolds, the entire community will have the opportunity to influence the plot and participate in key decision-making moments.",
+      link: "https://portal.arbitrum.io/projects/gaming?project=captain-laserhawk-the-game",
+      image:
+        "https://portal.arbitrum.io/_next/image?url=%2Fimages%2Fprojects%2Fcaptain-laserhawk-the-game-logo.webp&w=256&q=75",
+    },
+    {
+      commingsoon: true,
+      title: "Celeros",
+      description:
+        "The future of racing - A world built on risk and reward, brought to life through the competition of racing, trading, owning, & creating.",
+      link: "https://portal.arbitrum.io/projects/gaming?project=celeros",
+      image:
+        "https://portal.arbitrum.io/_next/image?url=%2Fimages%2Fprojects%2Fceleros-logo.webp&w=256&q=75",
+    },
+    {
+      title: "Counter Fire",
+      description:
+        "Delivering an intense, fast-paced MOBA gaming experience on your phone, fueled by blockchain assets.",
+      link: "https://portal.arbitrum.io/projects/gaming?project=counter-fire",
+      image:
+        "https://portal.arbitrum.io/_next/image?url=%2Fimages%2Fprojects%2Fcounter-fire-logo.webp&w=256&q=75",
+    },
+    {
+      commingsoon: true,
+      title: "Dookey Dash: Unclogged",
+      description:
+        "The viral endless runner is back! From the idiots who brought you Bored Ape Yacht Club comes the much anticipated game, Dookey Dash: Unclogged. Dive into the sewers, climb leaderboards, and win prizes from a seasonal pool of over $1 million.",
+      link: "https://portal.arbitrum.io/projects/gaming?project=dookey-dash-unclogged",
+      image:
+        "https://portal.arbitrum.io/_next/image?url=%2Fimages%2Fprojects%2Fdookey-dash-unclogged-logo.webp&w=256&q=75",
+    },
+  ],
 };
